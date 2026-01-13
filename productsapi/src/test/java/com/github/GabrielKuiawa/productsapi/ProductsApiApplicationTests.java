98e115b91625e60d25f2acff24490235d04191f4
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-01-13 05:46:43 -0300

    first commit

---
+++ productsapi/src/test/java/com/github/GabrielKuiawa/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.GabrielKuiawa.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
