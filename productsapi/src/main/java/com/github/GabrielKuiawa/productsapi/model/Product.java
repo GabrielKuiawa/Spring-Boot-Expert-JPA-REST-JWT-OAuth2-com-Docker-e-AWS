a1f04800c63c024d5a8347e82ca668357d7bf3c8
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-01-13 07:23:31 -0300

    Creating the JPA mapping for the Product entity

--- productsapi/src/main/java/com/github/GabrielKuiawa/productsapi/model/Product.java
+++ productsapi/src/main/java/com/github/GabrielKuiawa/productsapi/model/Product.java
@@ -1,7 +1,12 @@
 package com.github.GabrielKuiawa.productsapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
     private String id;
     private String name;
     private String description;
--- productsapi/src/main/resources/data.sql
+++ productsapi/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-create table products (
+create table Product (
     id          varchar(255) not null primary key,
     name        varchar(50)  not null,
     description varchar(300),
