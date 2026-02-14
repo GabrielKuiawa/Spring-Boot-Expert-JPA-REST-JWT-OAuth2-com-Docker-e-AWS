098792abb9525001094529111936b98630007369
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-02-14 16:11:20 -0300

    How to generate tables automatically with JPA

--- libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/model/Book.java
+++ libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/model/Book.java
@@ -3,6 +3,7 @@ package com.github.GabrielKuiawa.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -30,7 +31,7 @@ public class Book {
     private GenreBook genre;
 
     @Column(name = "price",precision = 18, scale = 2)
-    private Double price;
+    private BigDecimal price;
 
     @ManyToOne
     @JoinColumn(name = "id_author")
