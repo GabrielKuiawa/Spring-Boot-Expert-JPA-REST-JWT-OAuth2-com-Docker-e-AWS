b75bfc22e0daaa5a89cb76f7236d73a4d9bbd70b
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-02-13 23:53:25 -0300

    BeanManager

--- springarchitecture/src/main/java/com/github/GabrielKuiawa/springarchitecture/BeanManager.java
+++ springarchitecture/src/main/java/com/github/GabrielKuiawa/springarchitecture/BeanManager.java
@@ -3,9 +3,11 @@ package com.github.GabrielKuiawa.springarchitecture;
 import com.github.GabrielKuiawa.springarchitecture.todos.TodoEntity;
 import com.github.GabrielKuiawa.springarchitecture.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("request")
 public class BeanManager {
 
     @Autowired
