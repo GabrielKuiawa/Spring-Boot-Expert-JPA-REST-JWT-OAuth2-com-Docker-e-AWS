dc566d97b978e0efb30f0c85567bfb1f71846b25
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-03-09 17:15:26 -0300

    Creating an Exception Handler

---
+++ libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/controller/commom/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.github.GabrielKuiawa.libraryapi.controller.commom;
+
+import com.github.GabrielKuiawa.libraryapi.controller.dto.ResponseError;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<com.github.GabrielKuiawa.libraryapi.controller.dto.FieldError> listErrors =
+                fieldErrors.stream().map(fe ->
+                        new com.github.GabrielKuiawa.libraryapi.controller.dto.
+                                FieldError(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ResponseError(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Error validate.",
+                listErrors);
+    }
+}
