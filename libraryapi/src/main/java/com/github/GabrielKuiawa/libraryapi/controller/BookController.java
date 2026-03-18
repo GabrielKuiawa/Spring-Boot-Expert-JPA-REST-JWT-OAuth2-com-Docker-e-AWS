b0bd20c5087ab4dfba3f3f2c23b9d3d710f8d9d5
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-03-18 14:53:16 -0300

    Endpoint for book registration with Bean Validation

--- libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/controller/BookController.java
+++ libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/controller/BookController.java
@@ -1,7 +1,14 @@
 package com.github.GabrielKuiawa.libraryapi.controller;
 
+import com.github.GabrielKuiawa.libraryapi.controller.dto.BookRegistrationDTO;
+import com.github.GabrielKuiawa.libraryapi.controller.dto.ResponseError;
+import com.github.GabrielKuiawa.libraryapi.exceptions.DuplicateRegisterException;
 import com.github.GabrielKuiawa.libraryapi.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService service;
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody @Valid BookRegistrationDTO dto) {
+        try {
+            return ResponseEntity.ok(dto);
+        } catch (DuplicateRegisterException e) {
+            ResponseError errorDTO = ResponseError.conflict(e.getMessage());
+            return ResponseEntity.status(errorDTO.status()).body(errorDTO);
+        }
+    }
+
 }
--- libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/controller/dto/BookRegistrationDTO.java
+++ libraryapi/src/main/java/com/github/GabrielKuiawa/libraryapi/controller/dto/BookRegistrationDTO.java
@@ -1,16 +1,26 @@
 package com.github.GabrielKuiawa.libraryapi.controller.dto;
 
 import com.github.GabrielKuiawa.libraryapi.model.GenreBook;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record BookRegistrationDTO(
+        @NotBlank(message = "required field")
+        @ISBN
         String isbn,
+        @NotBlank(message = "required field")
         String title,
+        @NotNull(message = "required field")
+        @Past(message = "It cannot be a future date.")
         LocalDate publicationDate,
         GenreBook genre,
         BigDecimal price,
+        @NotNull(message = "required field")
         UUID idAuthor) {
 }
